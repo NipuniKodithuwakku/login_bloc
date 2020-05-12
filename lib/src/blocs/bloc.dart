e62a066d17582c38b9d252b3a524983c4e769bfa
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-13 04:52:50 +0530

    improving the bloc api

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,6 +1,19 @@
 import 'dart:async';
 
 class Bloc {
-  final emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
+  // final emailController = StreamController<String>();
+  // final passwordController = StreamController<String>();
+
+  // we named emailController and passwordController as email and password for the simplycity.
+  //also make it private
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  //Add data to stream
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  //retrieve data from stream
+  Stream<String> get email => _email.stream;
+  Stream<String> get password => _password.stream;
 }
